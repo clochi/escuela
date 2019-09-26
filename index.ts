9c30c0f882c471d67dc1296edcddbd5d7df306d7
Claudio Zurita <claudiozurita87@gmail.com>
2019-09-26 10:02:12 -0300

    Really necessary

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@ import { IConfig } from './interfaces';
 
 const config: IConfig = {
     auth: {
-        email: 'toronacii@gmail.com',
-        password: 'trni99'
+        email: 'youremail@account',
+        password: 'yourpassword'
     },
     videoNumber: 1,
     url: 'https://escuela.it/cursos/curso-de-analisis-y-diseno-orientados-a-objetos/clase/introduccion-al-analisis-y-diseno-orientados-a-objetos',
